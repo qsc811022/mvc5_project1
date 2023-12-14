7a7f1dd99b7a60003c4a9f1497160f93d0934377
劉敦桐 <zse811022@hotmail.com>
2023-12-14 20:40:10 +0800

    first_commit

---
+++ Web5Application/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+using Web5Application.Service;
+
+namespace Web5Application.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+		[HttpPost]
+		public async Task<ActionResult> Chat(JsonInputModel inputModel)
+		{
+			if (ModelState.IsValid)
+			{
+				var chatService = new ChatService(); // 或者通過依賴注入獲取
+				var response = await chatService.GetResponse(inputModel.Input);
+				return Content(response);
+			}
+			return Json(new { success = false, message = "Invalid input" });
+		}
+
+		public class JsonInputModel
+		{
+			public string Input { get; set; }
+		}
+	}
+}
---
+++ Web5Application/Service/ChatService.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Web5Application.Service
+{
+    public class ChatService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiUrl = "https://api.openai.com/v1/engines/davinci/completions"; // 示例 URL
+        private readonly string _apiKey = "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";
+
+        public ChatService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<string> GetResponse(string userInput)
+        {
+            var requestData = new
+            {
+                prompt = userInput,
+                max_tokens = 100 // 示例参数
+            };
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(_apiUrl),
+                Content = new StringContent(JsonConvert.SerializeObject(requestData), Encoding.UTF8, "application/json"),
+                Headers =
+            {
+                { "Authorization", $"Bearer {_apiKey}" }
+            }
+            };
+
+            try
+            {
+                var response = await _httpClient.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                return content; // 解析并返回响应
+            }
+            catch (HttpRequestException e)
+            {
+                // 适当的错误处理
+                return $"Error: {e.Message}";
+            }
+        }
+    }
+
+}
